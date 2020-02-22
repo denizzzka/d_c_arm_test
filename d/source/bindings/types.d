b24a0ccab8ef92cdf1890800d2d0ac57ecc01949
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-02-23 02:11:45 +0700

    version ARM for types

--- d/source/bindings/types.d
+++ d/source/bindings/types.d
@@ -1,6 +1,8 @@
 ///Types not avail from core.stdc.config for bare-metal
 module bindings.types;
 
-// ARM types:
-alias c_long = int;
-alias c_ulong = uint;
+version (ARM)
+{
+    alias c_long = int;
+    alias c_ulong = uint;
+}
