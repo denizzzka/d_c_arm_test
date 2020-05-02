8cc0bddf78584d0ef340f23d603cab9907ec3943
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-05-03 04:17:17 +0700

    __tls_get_addr placeholder added

--- d/stm32_druntime_backend/external/rt/sections.d
+++ d/stm32_druntime_backend/external/rt/sections.d
@@ -4,3 +4,8 @@ void[] externalInitTLSRanges() nothrow @nogc
 {
     assert(false, "not implemented");
 }
+
+extern(C) void* __tls_get_addr(void*) nothrow @nogc
+{
+    assert(false, "not implemented");
+}
