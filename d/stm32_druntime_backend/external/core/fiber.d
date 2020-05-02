0b257ba64a26fac49a6acdcd5f5a2989cf6b1183
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-05-02 22:00:31 +0700

    fiber_switchContext link fix

--- d/stm32_druntime_backend/external/core/fiber.d
+++ d/stm32_druntime_backend/external/core/fiber.d
@@ -12,7 +12,7 @@ export void fiber_entryPoint() nothrow /* LDC */ @assumeUsed
     assert(false, "Not implemented");
 }
 
-export void fiber_switchContext( void** oldp, void* newp ) nothrow @nogc
+extern (C) void fiber_switchContext( void** oldp, void* newp ) nothrow @nogc
 {
     assert(false, "Not implemented");
 }
