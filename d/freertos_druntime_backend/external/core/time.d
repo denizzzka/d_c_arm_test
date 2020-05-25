502f295615dbd94a52652d7eef356f377e4d5c66
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-05-26 02:53:37 +0700

    gettimeofday stub

--- d/freertos_druntime_backend/external/core/time.d
+++ d/freertos_druntime_backend/external/core/time.d
@@ -24,3 +24,18 @@ void initTicksPerSecond(ref long[] tps)
 
     tps[0] = ticksPerSecond; // ClockType.normal
 }
+
+// Linked by picolibc
+struct timeval {
+    long    tv_sec;     /* seconds */
+    long    tv_usec;    /* and microseconds */
+}
+
+extern(C) int gettimeofday(timeval* tv, void*) // timezone_t* is normally void*
+{
+    //FIXME
+    tv.tv_sec = 123;
+    tv.tv_usec = 456;
+
+    return 0;
+}
