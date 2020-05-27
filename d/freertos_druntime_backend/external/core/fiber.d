ba1ce7049f1b09bb122a3272f39013acd2e2d6f9
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-05-28 03:24:23 +0700

    asm: psp -> sp fix

--- d/freertos_druntime_backend/external/core/fiber.d
+++ d/freertos_druntime_backend/external/core/fiber.d
@@ -16,30 +16,31 @@ export void fiber_entryPoint() nothrow /* LDC */ @assumeUsed @nogc
 
 extern (C) void fiber_switchContext(size_t** for_store_curr_sp, size_t* switch_to_sp) nothrow @nogc @naked
 {
-	import ldc.llvmasm;
+    import ldc.llvmasm;
 
-	__asm!()(`
-	// stack grows down (sp is decrementing while grow)
-	PUSH    {r0-r12}                @ Push user registers (except two what contain arguments)
+    __asm!()(`
+    // stack grows down (sp is decrementing while grow)
+    PUSH    {r0-r12}                @ Push user registers (except two what contain arguments)
 
-	@FIXME:
+    @FIXME:
     @MRS     r0, SPSR                @ Pick up Saved Program Status Register
     PUSH    {r0, lr}                @ and push it with return address.
 
-	LDRH	r1, [$0]                @ Get pointer to storage ptr
-	MRS		r2, psp                 @ Get and
-	STRH	r2, [r1]                @ store sp value into storage
+    LDRH    r1, [$0]                @ Get pointer to storage ptr
+    MOV     r2, sp                  @ Get sp and
+    STRH    r2, [r1]                @ store its value into storage
 
     LDRH    r3, [$1]                @ Load new process sp from switch_to_sp argument
-	MSR		psp, r3                 @ Set up new sp value
+    MOV     sp, r3                  @ Set up new sp value
 
     POP     {r0, lr}                @ Pop SPSR data
+    @FIXME:
     @MSR     SPSR_cxsf, r0           @ and restore some SPSR fields.
 
     POP     {r0-r12}                @ Pop the rest of the registers
 
-	BX      lr						@ Return (also sets PC to new value)
-	`,
-	"r,r",
-	for_store_curr_sp, switch_to_sp);
+    BX      lr                      @ Return (also sets PC to new value)
+    `,
+    "r,r",
+    for_store_curr_sp, switch_to_sp);
 }
