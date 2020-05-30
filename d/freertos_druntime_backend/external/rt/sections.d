1baec441727147cffc615eb3c4610ac3e1b581b2
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-05-31 03:49:10 +0700

    fillGlobalSectionGroup works

--- d/freertos_druntime_backend/external/rt/sections.d
+++ d/freertos_druntime_backend/external/rt/sections.d
@@ -8,15 +8,16 @@ void[] externalInitTLSRanges() nothrow @nogc
 import rt.sections_ldc : SectionGroup;
 
 // These values described in linker script
-private extern (C) extern __gshared void* _data;
-private extern (C) extern __gshared void* _etbss;
+extern(C) extern __gshared void* _data;
+extern(C) extern __gshared void* _ebss;
 
 void fillGlobalSectionGroup(ref SectionGroup gsg) nothrow @nogc
 {
     // Writeable data segments covered by GC, from .data to end of .tbss
-    size_t size = _etbss - _data;
+    auto data_start = cast(void*)&_data;
+    ptrdiff_t size = cast(void*)&_ebss - data_start;
 
-    gsg._gcRanges.insertBack(_data[0 .. size]);
+    gsg._gcRanges.insertBack(data_start[0 .. size]);
 }
 
 extern(C) void* __tls_get_addr(void*) nothrow @nogc
