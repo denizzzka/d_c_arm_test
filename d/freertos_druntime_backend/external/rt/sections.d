59ddcab26969a34031b8cd2e24cc3e7e99c6b9e2
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-05-31 15:10:49 +0700

    initTLSRanges stub, _tlsSize fill out

--- d/freertos_druntime_backend/external/rt/sections.d
+++ d/freertos_druntime_backend/external/rt/sections.d
@@ -1,26 +1,30 @@
 module  external.rt.sections;
 
-void[] externalInitTLSRanges() nothrow @nogc
-{
-    assert(false, "not implemented");
-}
-
 import rt.sections_ldc : SectionGroup;
 
 // These values described in linker script
 extern(C) extern __gshared void* _data;
 extern(C) extern __gshared void* _ebss;
 
+extern(C) extern __gshared void* _tdata;
+extern(C) extern __gshared void* _etbss;
+
 void fillGlobalSectionGroup(ref SectionGroup gsg) nothrow @nogc
 {
-    // Writeable data segments covered by GC, from .data to end of .tbss
+    // Writeable data sections covered by GC
     auto data_start = cast(void*)&_data;
-    ptrdiff_t size = cast(void*)&_ebss - data_start;
+    ptrdiff_t size = cast(void*)&_ebss - _data;
 
     gsg._gcRanges.insertBack(data_start[0 .. size]);
+
+    // TLS data sections
+    gsg._tlsSize = cast(void*)&_etbss - _tdata;
 }
 
-extern(C) void* __tls_get_addr(void*) nothrow @nogc
+/***
+ * Called once per thread; returns array of thread local storage ranges
+ */
+void[] initTLSRanges() nothrow @nogc
 {
     assert(false, "not implemented");
 }
