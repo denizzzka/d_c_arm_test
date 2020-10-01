d1af633c7184f66b43a73a83d2c025bea69b027c
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-10-01 07:23:57 +0700

    our ouwn D abort() implementation what calls llvm_debugtrap loop

--- d/freertos_druntime_backend/external/core/abort.d
+++ d/freertos_druntime_backend/external/core/abort.d
@@ -2,5 +2,11 @@ module external.core.abort;
 
 export void abort(scope string msg, scope string filename = __FILE__, size_t line = __LINE__) @nogc nothrow @safe
 {
-    assert(false, "Not implemented");
+    import ldc.intrinsics;
+
+    // Replaces unimplemented ldc2 --checkaction=halt_debug option
+    // https://github.com/ldc-developers/ldc/issues/3454
+
+    while(true)
+        llvm_debugtrap();
 }
