0b43a4054d300fa5cd6b77f072caf3fa46096a83
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-10-25 20:26:01 +0700

    HFSR and CFSR added to debug

--- d/source/arm_cortex_debug.d
+++ d/source/arm_cortex_debug.d
@@ -3,6 +3,19 @@ module arm_cortex_debug;
 version (LDC):
 version (ARM_Thumb):
 
+/* --- ARM Cortex-M0, M3 and M4 specific definitions ----------------------- */
+/* Private peripheral bus - Internal */
+//FIXME: get from libopencm3
+enum PPBI_BASE = 0xE0000000U;
+enum SCS_BASE = PPBI_BASE + 0xE000;
+enum SCB_BASE = SCS_BASE + 0x0D00;
+
+/** CFSR: Configurable Fault Status Registers */
+enum SCB_CFSR = SCB_BASE + 0x28;
+
+/** HFSR: Hard Fault Status Register */
+enum SCB_HFSR = SCB_BASE + 0x2C;
+
 nothrow:
 @nogc:
 
@@ -34,6 +47,9 @@ extern(C) void hardFaultHandler() @naked
 
     regs.sp += InterruptStackFrame.sizeof;
 
+    regs.hfsr = *(cast(HFSR*) SCB_HFSR);
+    regs.cfsr = *(cast(CFSR*) SCB_CFSR);
+
     while(true)
         llvm_debugtrap();
 }
@@ -52,10 +68,59 @@ struct InterruptStackFrame
 
 struct Registers
 {
-    void* sp;
-
     InterruptStackFrame other;
     alias other this;
+
+    void* sp;
+
+    HFSR hfsr; /// Hard Fault Status Register
+    CFSR cfsr; /// Configurable Fault Status Registers
+}
+
+import std.bitmanip;
+
+struct HFSR
+{
+    mixin(bitfields!(
+        bool, "reserved", 1,
+        bool, "VECTTBL",  1,
+        uint, "reserved", 28,
+        bool, "FORCED",   1,
+        bool, "DEBUGEVT", 1,
+    ));
+}
+
+struct CFSR
+{
+    mixin(bitfields!(
+        // MMFSR
+        bool, "MMFSR_IACCVIOL", 1,
+        bool, "MMFSR_DACCVIOL", 1,
+        bool, "MMFSR_reserved", 1,
+        bool, "MMFSR_MUNSTKERR", 1,
+        bool, "MMFSR_MSTKERR", 1,
+        ushort, "MMFSR_reserved", 2,
+        bool, "MMFSR_MMARVALID", 1,
+
+        // BFSR
+        bool, "BFSR_IBUSERR", 1,
+        bool, "BFSR_PRECISERR", 1,
+        bool, "BFSR_IMPRECISERR", 1,
+        bool, "BFSR_UNSTKERR", 1,
+        bool, "BFSR_STKERR", 1,
+        ushort, "BFSR_reserved", 2,
+        bool, "BFSR_BFARVALID", 1,
+
+        // UFSR
+        bool, "UFSR_UNDEFINSTR", 1,
+        bool, "UFSR_INVSTATE", 1,
+        bool, "UFSR_INVPC", 1,
+        bool, "UFSR_NOCP", 1,
+        ubyte, "UFSR_reserved0", 4,
+        bool, "UFSR_UNALIGNED", 1,
+        bool, "UFSR_DIVBYZERO", 1,
+        ubyte, "UFSR_reserved1", 6,
+    ));
 }
 
 void doHardFault()
