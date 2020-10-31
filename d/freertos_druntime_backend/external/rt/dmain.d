76acb2a97a2477f098d4fd381f0432d78e285a54
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-11-01 04:37:46 +0700

    exitFromUnittests() disabled

--- d/freertos_druntime_backend/external/rt/dmain.d
+++ d/freertos_druntime_backend/external/rt/dmain.d
@@ -4,15 +4,15 @@ void createCLIargs(int argc, char** argv, out char[][] args, out size_t totalArg
 {
 }
 
-import ldc.attributes;
+//~ import ldc.attributes;
 
 // Graceful exit from Qemu or semihosting if program is tested or call hardware fault in other case
-@section(".fini_array")
-immutable exitFromUnittests_ptr = &exitFromUnittests;
+//~ @section(".fini_array")
+//~ immutable exitFromUnittests_ptr = &exitFromUnittests;
 
-extern(C) void exitFromUnittests()
-{
-    _exit(0);
-}
+//~ extern(C) void exitFromUnittests()
+//~ {
+    //~ _exit(0);
+//~ }
 
-extern(C) void _exit(int) nothrow @nogc;
+//~ extern(C) void _exit(int) nothrow @nogc;
