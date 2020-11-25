6923c209ac1a86778fdc7535594a8f46437faf05
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-11-25 23:05:26 +0700

    gpio -> libopencm3, spi support added

--- d/source/app.d
+++ d/source/app.d
@@ -1,9 +1,9 @@
-import gpio;
+import libopencm3;
 import freertos;
 
 int main()
 {
-	gpio_setup();
+	//~ blink_gpio_setup();
 
 	//~ xTaskCreate(
 		//~ &blinkTask,
@@ -14,17 +14,48 @@ int main()
 		//~ null // task handler
 	//~ );
 
-	//~ vTaskStartScheduler();
+	initDisplay();
 
-	// Will not get here unless there is insufficient RAM
-	return 1;
+	while(true)
+	{
+		sendToSegmentDisplay(SegmentCmd.DisplayTest, 0b1);
+		vTaskDelay(500);
+		sendToSegmentDisplay(SegmentCmd.DisplayTest, 0);
+		vTaskDelay(500);
+	}
 }
 
 extern(C) void blinkTask(void *pvParametres) @nogc nothrow
 {
-	gpio_toggle(GPIOB, GPIO1);
+	gpio_toggle(GPIO_PORT_B_BASE, GPIO1);
 
 	vTaskDelay(500);
 }
 
-extern(C) __gshared string[] rt_options = [ "gcopt=minPoolSize:16K maxPoolSize:3M incPoolSize:4K" ];
+enum SegmentCmd : ubyte
+{
+	NoOp = 0,
+	DecodeMode =  0b1001,
+	Intensity =   0b1010,
+	ScanLimit =   0b1011,
+	Shutdown =    0b1100,
+	DisplayTest = 0b1111,
+}
+
+void initDisplay()
+{
+	spi_setup();
+
+	// display all digits
+	sendToSegmentDisplay(SegmentCmd.ScanLimit, 0b111);
+
+	// set maximum intensity
+	sendToSegmentDisplay(SegmentCmd.Intensity, 0b1111);
+}
+
+private void sendToSegmentDisplay(SegmentCmd code, ubyte data)
+{
+	ushort arg = cast(ushort) code << 8 | data;
+
+	sendDataToSegmentDisplay(arg);
+}
