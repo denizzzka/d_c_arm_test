7c11a0637dbd28e86c89a16f0fe6ed1db6a2cd67
Denis Feklushkin <denis.feklushkin@gmail.com>
2021-01-24 16:50:13 +0700

    forgot to add external/core/gc.d

---
+++ d/freertos_druntime_backend/external/core/gc.d
@@ -0,0 +1,24 @@
+module external.core.gc;
+
+import core.internal.parseoptions;
+
+struct Config
+{
+    bool disable;            // start disabled
+    ubyte profile;           // enable profiling with summary when terminating program
+    string gc = "conservative"; // select gc implementation conservative|precise|manual
+
+    size_t initReserve;      // initial reserve (bytes)
+    size_t minPoolSize = 16 * 1024;  // initial and minimum pool size
+    size_t maxPoolSize = 3 * 1024 * 1024;  // maximum pool size
+    size_t incPoolSize = 4 * 1024;  // pool size increment
+
+    uint parallel = 10;      // number of additional threads for marking (limited by cpuid.threadsPerCPU-1)
+    float heapSizeFactor = 2.0; // heap size to used memory ratio
+    string cleanup = "collect"; // select gc cleanup method none|collect|finalize
+
+    bool initialize() @nogc nothrow
+    {
+        return true;
+    }
+}
