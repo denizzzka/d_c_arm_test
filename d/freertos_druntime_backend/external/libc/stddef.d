bd8806b867ca29ec4d4a12811827c3070e1d773f
Denis Feklushkin <denis.feklushkin@gmail.com>
2021-03-15 15:53:42 +0700

    backend: druntime fenv.d support added

--- d/freertos_druntime_backend/external/libc/stddef.d
+++ d/freertos_druntime_backend/external/libc/stddef.d
@@ -1,3 +1,14 @@
 module external.libc.stddef;
 
 alias wchar_t = dchar;
+
+// fenv.d content:
+struct fenv_t
+{
+    c_ulong __cw;
+}
+
+import core.stdc.config: c_ulong;
+alias fexcept_t = c_ulong;
+
+enum FE_DFL_ENV = cast(fenv_t*)(-1);
