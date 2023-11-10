bfbe4082912bedcb20b3a68504ad560ed8e1fe35
Denis Feklushkin <feklushkin.denis@gmail.com>
2023-11-10 20:00:25 +0300

    extern(C) for abort(), added to meson

--- d/druntime_freertos_backend/external/core/abort.d
+++ d/druntime_freertos_backend/external/core/abort.d
@@ -1,6 +1,6 @@
 module external.core.abort;
 
-export void abort(scope string msg, scope string filename = __FILE__, size_t line = __LINE__) @nogc nothrow @safe
+extern(C) export void abort(scope string msg, scope string filename = __FILE__, size_t line = __LINE__) @nogc nothrow @safe
 {
     import ldc.intrinsics;
 
