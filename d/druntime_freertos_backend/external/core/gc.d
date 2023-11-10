1d55cd4d89ede110e4e3bd977958b03d38e93b5d
Denis Feklushkin <feklushkin.denis@gmail.com>
2023-11-10 21:30:02 +0300

    initConfigOptions fixed

--- d/druntime_freertos_backend/external/core/gc.d
+++ d/druntime_freertos_backend/external/core/gc.d
@@ -1,34 +1,21 @@
 module external.core.gc;
 
-import core.gc.config : CFG = Config;
+import core.gc.config : Config;
 
-extern(C) export bool initConfigOptions(ref CFG cfg, string cfgname)
+extern(C) export bool initConfigOptions(ref Config cfg, string cfgname)
 {
-    cfg = Config;
+    Config c = {
+        fork: false,        // optional concurrent behaviour
+        gc: "conservative", // select gc implementation conservative|precise|manual
+        minPoolSize: 16 * 1024,       // initial and minimum pool size
+        maxPoolSize: 3 * 1024 * 1024, // maximum pool size
+        incPoolSize: 4 * 1024,        // pool size increment
+        parallel: 10,        // number of additional threads for marking (limited by cpuid.threadsPerCPU-1)
+        heapSizeFactor: 2.0, // heap size to used memory ratio
+        cleanup: "collect",  // select gc cleanup method none|collect|finalize
+    };
+
+    cfg = c;
 
     return false;
-}
-
-import core.internal.parseoptions;
-
-struct Config
-{
-    bool disable;            // start disabled
-    bool fork = false;       // optional concurrent behaviour
-    ubyte profile;           // enable profiling with summary when terminating program
-    string gc = "conservative"; // select gc implementation conservative|precise|manual
-
-    size_t initReserve;      // initial reserve (bytes)
-    size_t minPoolSize = 16 * 1024;  // initial and minimum pool size
-    size_t maxPoolSize = 3 * 1024 * 1024;  // maximum pool size
-    size_t incPoolSize = 4 * 1024;  // pool size increment
-
-    uint parallel = 10;      // number of additional threads for marking (limited by cpuid.threadsPerCPU-1)
-    float heapSizeFactor = 2.0; // heap size to used memory ratio
-    string cleanup = "collect"; // select gc cleanup method none|collect|finalize
-
-    bool initialize() @nogc nothrow
-    {
-        return true;
-    }
 }
