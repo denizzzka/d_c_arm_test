f965f8d4e9bd70300a6cd2d6891c6df36321da92
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-08-04 03:31:19 +0300

    FreeRTOS: main() exit fixed in dfruntime

--- d/source/app.d
+++ d/source/app.d
@@ -1,13 +1,11 @@
 import core.exception;
 
-void main()
+int main()
 {
     // To ensure what this is not betterC
     try
         throw new Exception("test");
     catch(Exception) {}
 
-    // FIXME: return from main() broken at least for FreeRTOS
-    import core.stdc.stdlib: exit;
-    exit(0);
+    return 0;
 }
